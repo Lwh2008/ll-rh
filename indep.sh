5199d4c73a346bc3423a884e91cadb8e89fa4683
Lwh2008 <lwh20082021@outlook.com>
2024-06-29 13:14:56 +0800

    h

--- indep.sh
+++ indep.sh
@@ -3,8 +3,11 @@ sudo dnf up
 sudo dnf in -y qt5-qtbase-devel qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel ostree-devel yaml-cpp-devel systemd-devel libseccomp-devel docopt-cpp-devel python3-docopt elfutils-libelf-devel
 ln -s /usr/lib64/libdocopt.so /usr/lib64/libdocopt.a
 tar xjvf linglong.tar.bz
-cd linglong/build
+cd linglong
+mkdir build
+cmake ..
+make -j$(nproc)
 sudo make install
-sudo cp ../org.deepin.linglong.PacmageManager.service /etc/systemd/system
+sudo cp ../../org.deepin.linglong.PacmageManager.service /etc/systemd/system
 sudo systemctl enable --now org.deepin.linglong.PackageManager.service
 cat /etc/os-release

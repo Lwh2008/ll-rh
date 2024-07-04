6552e43579fdee5617a03bec949969e6bd5d6965
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-07-04 21:49:03 +0800

    更新 indep.sh

--- indep.sh
+++ indep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo dnf makecache
-sudo dnf up
-sudo dnf in -y qt5-qtbase-devel qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel ostree-devel yaml-cpp-devel systemd-devel libseccomp-devel docopt-cpp-devel python3-docopt elfutils-libelf-devel glibc-devel.x86_64 libstdc++-devel.x86_64 gcc gcc-c++
+dnf makecache
+dnf up
+dnf in -y qt5-qtbase-devel qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel ostree-devel yaml-cpp-devel systemd-devel libseccomp-devel docopt-cpp-devel python3-docopt elfutils-libelf-devel glibc-devel.x86_64 libstdc++-devel.x86_64 gcc gcc-c++
 ln -s /usr/lib64/libdocopt.so /usr/lib64/libdocopt.a
 tar xzvf linglong.tar.gz
 cd linglong

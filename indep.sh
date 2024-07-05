29115844a7ca69e80a4b39ea0403640993ec5a91
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-07-06 05:49:06 +0800

    更新 indep.sh

--- indep.sh
+++ indep.sh
@@ -4,12 +4,13 @@ dnf up
 dnf in -y cmake gcc-c++ qt5-qtbase-devel qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel nlohmann-json-devel ostree-devel yaml-cpp-devel systemd-devel gtest-devel libseccomp-devel elfutils-libelf-devel glibc-static libstdc++-static
 ln -s /usr/lib64/libdocopt.so /usr/lib64/libdocopt.a
 tar xzvf linglong.tar.gz
+ls
 cd linglong
+ls
 mkdir build
 cd build
 cmake ..
 make -j$(nproc)
 make install
-cp ../../org.deepin.linglong.PacmageManager.service /etc/systemd/system
-systemctl enable --now org.deepin.linglong.PackageManager.service
-cat /etc/os-release
+#cp ../../org.deepin.linglong.PacmageManager.service /etc/systemd/system
+#systemctl enable --now org.deepin.linglong.PackageManager.service

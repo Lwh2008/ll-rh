15b192648b6226a544b4b5c8b645bdfabd34a024
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-07-07 17:41:19 +0800

    更新 indep.sh

--- indep.sh
+++ indep.sh
@@ -2,12 +2,11 @@
 #dnf in -y epel-release
 dnf makecache -y
 dnf up -y
-dnf in -y cmake gcc-c++ qt5-qtbase* qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel nlohmann-json-devel ostree-devel yaml-cpp-devel systemd-devel gtest-devel libseccomp-devel elfutils-libelf-devel glibc-static libstdc++-static qt5 docopt-cpp-devel
+dnf in -y cmake gcc-c++ qt5-qtbase-devel qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel nlohmann-json-devel ostree-devel yaml-cpp-devel systemd-devel gtest-devel libseccomp-devel elfutils-libelf-devel glibc-static libstdc++-static docopt-cpp-devel
 ln -s /usr/lib64/libdocopt.so /usr/lib64/libdocopt.a
 tar xzf linglong.tgz
 cd linglong
 #cat ../cmake.add >> CMakeLists.txt
-mkdir build
 cd build
 #dnf se libqt5
 cmake ..

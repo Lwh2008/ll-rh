70d316b17c6675f8c3f6e49e71c7c4c92c5baf83
Lwh2008 <lwh20082021@outlook.com>
2024-07-07 15:31:13 +0800

    up

--- indep.sh
+++ indep.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-dnf makecache
+dnf in -y epel-release
+dnf makecache -y
 dnf up -y
 dnf in -y cmake gcc-c++ qt5-qtbase* qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel nlohmann-json-devel ostree-devel yaml-cpp-devel systemd-devel gtest-devel libseccomp-devel elfutils-libelf-devel glibc-static libstdc++-static qt5 docopt-cpp-devel
 ln -s /usr/lib64/libdocopt.so /usr/lib64/libdocopt.a
-tar xzvf linglong.tar.gz
+tar xzf linglong.tgz
 cd linglong
 #cat ../cmake.add >> CMakeLists.txt
 mkdir build
 cd build
-dnf se libqt5
+#dnf se libqt5
 cmake ..
 make -j$(nproc)
 make install

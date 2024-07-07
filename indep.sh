274389546740a91877cb023fbb68346f949d2489
Lwh2008 <lwh20082021@outlook.com>
2024-07-07 17:21:46 +0800

    h

--- indep.sh
+++ indep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dnf in -y epel-release
+#dnf in -y epel-release
 dnf makecache -y
 dnf up -y
 dnf in -y cmake gcc-c++ qt5-qtbase* qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel nlohmann-json-devel ostree-devel yaml-cpp-devel systemd-devel gtest-devel libseccomp-devel elfutils-libelf-devel glibc-static libstdc++-static qt5 docopt-cpp-devel

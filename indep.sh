db5edd4e144d34377816980c193c11b36792f0f0
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-07-11 21:01:38 +0800

    更新 indep.sh

--- indep.sh
+++ indep.sh
@@ -5,7 +5,7 @@ dnf up -y
 dnf in -y cmake gcc-c++ qt5-qtbase-devel qt5-qtwebsockets-devel qt5-qtbase-private-devel glib2-devel nlohmann-json-devel ostree-devel yaml-cpp-devel systemd-devel gtest-devel libseccomp-devel elfutils-libelf-devel glibc-static libstdc++-static docopt-cpp-devel
 ln -s /usr/lib64/libdocopt.so /usr/lib64/libdocopt.a
 git clone https://github.com/OpenAtomFoundation/linglong
-mv 1.5.7 linglong
+#mv 1.5.7 linglong
 cd linglong
 mkdir build
 cd build

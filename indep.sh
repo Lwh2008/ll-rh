3ec187484754c91429e1322b47834484989ee563
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-07-14 20:08:09 +0800

    更新 indep.sh

--- indep.sh
+++ indep.sh
@@ -12,6 +12,5 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 make
 make install
-ll-cli install org.deepin.Foundation
 #cp ../../org.deepin.linglong.PacmageManager.service /etc/systemd/system
 #systemctl enable --now org.deepin.linglong.PackageManager.service
